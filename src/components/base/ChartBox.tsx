a1a86807abd3d7ede87a0cbe55d9b64afad4d624
Mahan Kavian <kavianmahandvp@gmail.com>
2024-09-19 18:13:34 +0330

    edit PieChart Legend

--- src/components/base/ChartBox.tsx
+++ src/components/base/ChartBox.tsx
@@ -10,15 +10,20 @@ export function ChartBox({chartName, chart}: Props) {
     return (
         <div className={"rounded-md border grid grid-cols-1 p-3"}>
             <div className={"w-full justify-between flex items-center mb-6"}>
-                <span className={"capitalize text-dark-100 font-semibold text-sm"}>
-                    {chartName}
-                </span>
+                <div className={"flex flex-col gap-1"}>
+                    <span className={"capitalize text-dark-100 font-semibold text-sm"}>
+                        {chartName}
+                    </span>
+                    <span className={"text-xs text-neutral-100 hidden md:block"}>
+                        Description
+                    </span>
+                </div>
                 <i>{menuIcon}</i>
             </div>
             <>
                 {chart}
             </>
-            <div className={"w-full flex justify-end items-center"}>
+            <div className={"w-full flex justify-end md:justify-start md:mt-2 items-center"}>
                 <span className={"text-sm text-active font-semibold"}>View details</span>
             </div>
         </div>
--- src/components/base/charts/PieChartCustom.tsx
+++ src/components/base/charts/PieChartCustom.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { PieChart, Pie, Cell } from 'recharts';
+import {PieChart, Pie, Cell, Legend} from 'recharts';
 
 interface Props {
     percentage: number;
@@ -10,6 +10,20 @@ interface PieChartData {
     value: number;
 }
 
+const PiChartLegend = (props: any) => {
+    const { payload } = props;
+
+    return (
+        <ul className={"flex gap-2 text-sm justify-center p-0 m-0"}>
+            {payload.map((entry: any, index: number) => (
+                <li key={index.toString()} className={"hidden md:flex  text-dark-100 items-center gap-2"}>
+                    <div style={{backgroundColor: `${entry.color }`}} className={`w-[11px] h-[11px]`}> </div>
+                    <div>{entry.value}</div>
+                </li>
+            ))}
+        </ul>
+    );
+}
 
 export default function PieChartCustom({ percentage } : Props) {
     const [isClient, setIsClient] = useState<boolean>(false);
@@ -19,8 +33,8 @@ export default function PieChartCustom({ percentage } : Props) {
     }, [])
 
     const data: PieChartData[] = [
-        { name: 'Progress', value: percentage },
-        { name: 'Remaining', value: 100 - percentage },
+        { name: 'Label 1', value: percentage },
+        { name: 'Label 2', value: 100 - percentage },
     ];
 
     const COLORS: string[] = ['#00bcd4', '#e0e0e0'];
@@ -33,7 +47,7 @@ export default function PieChartCustom({ percentage } : Props) {
                     <div className={"text-neutral-100 font-semibold pb-1 border-b-2 border-transparent"}>30 days</div>
                     <div className={"text-neutral-100 font-semibold pb-1 border-b-2 border-transparent"}>60 days</div>
                 </div>
-                <PieChart width={150} height={150}>
+                <PieChart width={150} height={190}>
                     <Pie
                         data={data}
                         cx="50%"
@@ -46,9 +60,10 @@ export default function PieChartCustom({ percentage } : Props) {
                         dataKey="value"
                     >
                         {data.map((entry:PieChartData, index: number) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+                            <Cell key={index.toString()} fill={COLORS[index % COLORS.length]}/>
                         ))}
                     </Pie>
+                    <Legend content={PiChartLegend}/>
                 </PieChart>
                 <div
                     className={"absolute text-3xl font-semibold text-dark-100 top-[60%] left-[50%] -translate-1/2"}
@@ -58,10 +73,11 @@ export default function PieChartCustom({ percentage } : Props) {
                 >
                     {percentage}%
                 </div>
-                <div className={"w-[190px] justify-between mx-auto text-xs font-semibold text-dark-100-100 bottom-[155px] absolute flex items-center"}>
+                <div className={"w-[190px] justify-between mx-auto text-xs font-semibold text-dark-100-100 bottom-[170px] absolute flex items-center"}>
                     <div className={"text-center"}>0</div>
                     <div className={"text-center"}>100</div>
                 </div>
+
             </div>
     );
 };
